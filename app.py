7e71e37111a51b0b56d6918a95ccb8967300f2c6
frankie siino <frankie.siino@gmail.com>
2020-03-08 13:37:09 -0700

    Start keying into request params for routes

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'test'
+
+mongo = PyMongo(app)
+
+@app.route('/api/foods/all', methods=['GET'])
+def get_all_foods():
+  food = mongo.db.foods
+  output = []
+  for food in food.find():
+    output.append({'ndbno': food['ndbno'], 'name': food['name'], 'weight': food['weight'],
+                   'measure': food['measure'], 'nutruents': food['nutrients']})
+  return jsonify({'result': output})
+
+@app.route('/api/foods/search', methods=['GET'])
+def get_queried_foods():
+  food = mongo.db.foods
+  fieldsets = []
+  queries = []
+  fieldsets.append(request.args)
+  for el in fieldsets:
+    query = {
+        'nutrients': {
+            '$elemMatch': {
+                'nutrient': el['nutrient'],
+                'gm': {
+                    '$gt': el['min'],
+                    '$lte': el['max']
+                }
+            }
+        }
+    }
+    queries.append(query)
+
+  return jsonify({'fieldsets': fieldsets}, {'queries': queries})
+
+@app.route('/')
+def index():
+  return jsonify({'msg': 'hello world'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# Run server in watch mode:
+# FLASK_APP=app.py FLASK_ENV=development flask run --port=8000
