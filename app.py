d81fb3ca387335cd113d29bc477518815d8633eb
frankie siino <frankie.siino@gmail.com>
2020-03-09 15:22:19 -0700

    Point root dir to react with current_app, send_from_directory

--- app.py
+++ app.py
@@ -89,7 +89,8 @@ def get_queried_foods():
 
 @app.route('/')
 def index():
-    return send_from_directory(os.path.join(current_app.root_path, 'client', 'build'), 'index.html')
+    return send_from_directory(os.path.join(os.path.dirname(__file__), 'client', 'build'), 'index.html')
+    # return send_from_directory(os.path.join(current_app.root_path, 'client', 'build'), 'index.html')
   # return jsonify({'msg': 'hello world'})
 
 if __name__ == '__main__':
